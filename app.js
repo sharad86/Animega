817b8666ef8aa5cacd1b959305ed396baafb374c
sharad86 <y.sharad.t@gmail.com>
2020-07-04 19:32:45 +0530

    update dburl

--- app.js
+++ app.js
@@ -15,11 +15,13 @@ var flash = require("connect-flash");
 var commentRoutes = require("./routes/comments"),
     animeRoutes = require("./routes/animes"),
     indexRoutes = require("./routes/index");
-mongoose.connect("mongodb+srv://animega:8oPKUhoObBdbtzJo@cluster0.q8kbb.mongodb.net/animega?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true }).then(()=>{
-  console.log("connected to DB!");
-}).catch(err=> {
-  console.log("ERROR:", err.message);
-})
+    
+mongoose.connect(process.env.DATABASEURL);
+  //"mongodb+srv://animega:8oPKUhoObBdbtzJo@cluster0.q8kbb.mongodb.net/animega?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true }).then(()=>{
+  //console.log("connected to DB!");
+//}).catch(err=> {
+  //console.log("ERROR:", err.message);
+//})
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
