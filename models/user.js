4b006a196a905eb263cad4b89720c221ce226dc2
sharad86 <y.sharad.t@gmail.com>
2020-07-06 21:13:05 +0530

    add image choose option for user profile

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = new mongoose.Schema({
 	username: {type:String, unique:true, required:true},
 	password: String,
-	avatar: String,
+	image: String,
 	firstName: String,
 	lastName: String,
 	email: {type:String, unique:true, required:true},
